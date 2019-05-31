7376e830b01d9296a6b3b7f3e048ac118b3349c6
Michael Littleton <michaelglittleton@gmail.com>
2019-05-31 01:07:15 -0400

    Finished rps problem

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,13 +2,28 @@
 
 import sys
 
+
 def rock_paper_scissors(n):
-  pass 
+    options = [['rock'], ['paper'], ['scissors']]
+
+    def inner(n, arr=[[]], cache={}):
+        newArr = []
+        if n == 0:
+            return arr
+        else:
+            for opt in options:
+                for item in arr:
+                    newArr += [opt + item]
+        plays = inner(n-1, newArr)
+        cache[n] = plays
+        return plays
+
+    return inner(n)
 
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+    if len(sys.argv) > 1:
+        num_plays = int(sys.argv[1])
+        print(rock_paper_scissors(num_plays))
+    else:
+        print('Usage: rps.py [num_plays]')
